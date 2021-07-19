f7db6447135dd31b2d253c9b9d2a3c3e725a61c6
mioow <hescl@naver.com>
2021-07-20 07:56:26 +0900

    초기 커밋.

---
+++ file_module.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import json
+
+## 한 디렉토리의 이미지의 이름을 읽어 DataFrame으로 반환하는 함수
+
+def loadImage(path):
+
+    file_list = os.listdir(path)
+
+    img_list = pd.DataFrame(file_list,columns=['file_name'])
+
+    print(img_list)
+
+    return img_list
+
+## 해당 디렉토리에서 중복을 검사하고 이미지를 저장
+
+def saveImage(img, path):
+    path
---
+++ preprocessing.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import file_module
+import numpy as np
+
+'''
+def nothing(x):
+    pass
+'''
+
+img_dir = "E:/data/Item-Image"
+save_dir = "E:/data/masked-Image"
+
+img_list = file_module.loadImage(img_dir)
+
+for index, row in img_list.iterrows():
+
+    img_path = img_dir +'/'+ row['file_name']
+    save_path = save_dir +'/'+ row['file_name']
+
+    '''
+    cv.namedWindow('binary')
+    cv.createTrackbar('threshold', 'binary', 0, 255, nothing) 
+    cv.setTrackbarPos('threshold', 'binary', 230)
+    '''
+
+    img_color = cv.imread(img_path)
+    img_gray = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
+
+    while(True): 
+        
+        #low = cv.getTrackbarPos('threshold', 'binary') 
+
+        ret,img_binary = cv.threshold(img_gray, 230, 255, cv.THRESH_BINARY_INV)
+
+        #cv2.threshold( grayscale image, threshold_value, value, flag{cv2.THRESH_BINARY_INV : pixle 값이 threshold_value 보다 크면 0, 작으면 value로} )
+  
+        #cv.imshow('binary', img_binary)
+        #cv.imshow("color", img_color)
+        cv.imwrite(save_path, img_binary)
+
+        #if cv.waitKey(1)&0xFF == 27: # esc 누르면 닫음
+        break
+
+print("process complete")
